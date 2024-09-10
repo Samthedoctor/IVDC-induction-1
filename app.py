1f24cd1f899c1d79bbdcc6572fe395eff29b7751
Samthedoctor <prince.saumya05@gmail.com>
2024-09-10 21:56:17 +0530

    identifying lane areas

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
 backSub = cv2.createBackgroundSubtractorMOG2()
 foreground_mask = backSub.apply(blurred_image)
 edges = cv2.Canny(blurred_image, 50, 150)
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=100, minLineLength=100, maxLineGap=10)
+lane_image = np.zeros_like(image)
+
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(lane_image, (x1, y1), (x2, y2), (255, 255, 255), 3)
