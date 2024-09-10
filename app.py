b7a80c0f855634b8565f0e19acd8d5e7c5653390
Samthedoctor <prince.saumya05@gmail.com>
2024-09-10 21:52:13 +0530

    subtracting the background noise

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ import cv2
 image = cv2.imread('lane_image.jpg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+backSub = cv2.createBackgroundSubtractorMOG2()
+foreground_mask = backSub.apply(blurred_image)
